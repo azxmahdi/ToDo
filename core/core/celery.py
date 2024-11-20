d9139c7d44bd790b4d473d62a29e9ab896601ace
azxmahdi <azxmahdi22@gmail.com>
2024-11-20 21:08:37 +0330

    Update celery.py

--- core/core/celery.py
+++ core/core/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
-environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.setting.settings_prod')
+environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.setting.settings_dev')
 
 app = Celery('core')
 
